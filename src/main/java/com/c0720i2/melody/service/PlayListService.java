fc47ab08fda9ffac53dfcc8b15490bb7b921e3eb
conghoan21 <conghoan.hnc@gmail.com>
2021-01-14 10:28:56 +0700

    code api-playlist

---
+++ src/main/java/com/c0720i2/melody/service/PlayListService.java
@@ -0,0 +1,33 @@
+package com.c0720i2.melody.service;
+
+import com.c0720i2.melody.model.Playlist;
+import com.c0720i2.melody.repository.PlayListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class PlayListService implements IPlayListService{
+    @Autowired
+    private PlayListRepository playListRepository;
+
+    @Override
+    public Iterable<Playlist> getAll() {
+        return playListRepository.findAll();
+    }
+
+    @Override
+    public Playlist save(Playlist playlist) {
+        return playListRepository.save(playlist);
+    }
+
+    @Override
+    public Optional<Playlist> findById(Long id) {
+        return playListRepository.findById(id);
+    }
+
+    @Override
+    public void remove(Long id) {
+        playListRepository.deleteById(id);
+    }
+}
