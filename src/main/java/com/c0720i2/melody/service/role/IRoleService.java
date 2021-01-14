0ed04e1d0cf086973bc182cb52c67cd75cf212bf
Son Nguyen <khacsonyhct@gmail.com>
2021-01-14 10:10:58 +0700

    add new

---
+++ src/main/java/com/c0720i2/melody/service/role/IRoleService.java
@@ -0,0 +1,7 @@
+package com.c0720i2.melody.service.role;
+
+import com.c0720i2.melody.model.Role;
+import com.c0720i2.melody.service.IGeneralService;
+
+public interface IRoleService extends IGeneralService<Role> {
+}
