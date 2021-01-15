abc02637e7d2fea252b12762fc055061c91875d4
conghoan21 <conghoan.hnc@gmail.com>
2021-01-15 09:26:29 +0700

    fix userdetail

--- src/main/java/com/c0720i2/melody/controller/AuthController.java
+++ src/main/java/com/c0720i2/melody/controller/AuthController.java
@@ -1,11 +1,10 @@
 package com.c0720i2.melody.controller;
-
-import com.c0720i2.melody.model.Guest;
 import com.c0720i2.melody.model.JwtResponse;
 import com.c0720i2.melody.model.Role;
 import com.c0720i2.melody.model.User;
+import com.c0720i2.melody.model.UserDetail;
 import com.c0720i2.melody.service.JwtService;
-import com.c0720i2.melody.service.guest.IGuestService;
+import com.c0720i2.melody.service.userdetail.IUserDetailService;
 import com.c0720i2.melody.service.user.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,26 +15,19 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.HashSet;
 import java.util.Set;
-
-
 @CrossOrigin("*")
 @RestController
 public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
-
     @Autowired
     private JwtService jwtService;
-
     @Autowired
     private IUserService userService;
-
     @Autowired
-    private IGuestService guestService;
-
+    private IUserDetailService guestService;
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody User user){
         Authentication authentication = authenticationManager.authenticate(
@@ -45,19 +37,26 @@ public class AuthController {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getUsername(),userDetails.getAuthorities()));
     }
-
-    @GetMapping("profile")
+    @GetMapping("/profile")
     public ResponseEntity<String> profile(){
         return new ResponseEntity<>("Profile", HttpStatus.OK);
     }
-
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody User user,@RequestBody Guest guest){
-        Set<Role> roleSet = new HashSet<>();
-        roleSet.add(new Role(2L,"ROLE_USER"));
-        user.setRoles(roleSet);
-        guestService.save(guest);
-        userService.save(user);
+    public ResponseEntity<User> register(@RequestBody User user){
+        if (user != null){
+            Set<Role> roleSet = new HashSet<>();
+            roleSet.add(new Role(2L,"ROLE_USER"));
+            user.setRoles(roleSet);
+            userService.save(user);
+        }
+//        if (userDetail != null){
+//            guestService.save(userDetail);
+//        }
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
+
+
+
+
+
