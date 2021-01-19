145d2796e51e98d03f87d4f72db2a2791a8cd98b
conghoan21 <conghoan.hnc@gmail.com>
2021-01-19 10:08:12 +0700

    add file

--- src/main/java/com/c0720i2/melody/repository/PlayListRepository.java
+++ src/main/java/com/c0720i2/melody/repository/PlayListRepository.java
@@ -1,8 +1,13 @@
 package com.c0720i2.melody.repository;
 
 import com.c0720i2.melody.model.Playlist;
+import com.c0720i2.melody.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PlayListRepository extends JpaRepository<Playlist, Long> {
+    Iterable<Playlist> findAllByUserUsername(String username);
 
+    @Query(value = "select * from playlist order by creation_time desc limit 10", nativeQuery = true)
+    Iterable<Playlist> findAllByCreationTimeOrderByCreationTime();
 }
--- src/main/java/com/c0720i2/melody/service/playlist/IPlayListService.java
+++ src/main/java/com/c0720i2/melody/service/playlist/IPlayListService.java
@@ -1,6 +1,7 @@
 package com.c0720i2.melody.service.playlist;
 
 import com.c0720i2.melody.model.Playlist;
+import com.c0720i2.melody.model.User;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,4 +12,7 @@ public interface IPlayListService {
     Playlist save (Playlist playlist);
     Optional<Playlist> findById(Long id);
     void remove(Long id);
+    Iterable<Playlist> findAllByUserUsername(String username);
+    Playlist addSongToPlaylist(Long idSong, Long idPlaylist);
+    Iterable<Playlist> listLatest();
 }
