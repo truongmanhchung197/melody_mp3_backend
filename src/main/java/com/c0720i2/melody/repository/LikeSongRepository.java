ecea777611fbed0a644fa20b8bef6df6d680b29e
Chung <truongmanhchung207@gmail.com>
2021-01-20 14:04:17 +0700

    add LikeSongRepository

--- src/main/java/com/c0720i2/melody/repository/LikeSongRepository.java
+++ src/main/java/com/c0720i2/melody/repository/LikeSongRepository.java
@@ -1,12 +1,15 @@
 package com.c0720i2.melody.repository;
 
+import com.c0720i2.melody.model.LikeSong;
+import com.c0720i2.melody.model.LikeSongId;
 import com.c0720i2.melody.model.Song;
+import com.c0720i2.melody.model.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.math.BigInteger;
 
-public interface LikeSongRepository extends CrudRepository<Song, Long> {
+public interface LikeSongRepository extends CrudRepository<LikeSong, LikeSongId> {
     @Query(value = "select id from song\n" +
             "    join (select count(user_id) as like_number, song_id\n" +
             "    from like_song group by song_id order by like_number desc)\n" +
